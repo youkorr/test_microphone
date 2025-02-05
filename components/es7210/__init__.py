7aa9a9e5bf3b07a458aa96bab1c41d1f17da1a3b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-05 18:42:40 +0100

    Update __init__.py

--- components/es7210/__init__.py
+++ components/es7210/__init__.py
@@ -2,8 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, microphone
 
+CODEOWNERS = ["@youkorr"]
 DEPENDENCIES = ['i2c']
 MULTI_CONF = True
+
+CONF_SAMPLE_RATE = 'sample_rate'
+CONF_BITS_PER_SAMPLE = 'bits_per_sample'
 
 es7210_ns = cg.esphome_ns.namespace('es7210')
 ES7210Component = es7210_ns.class_('ES7210Component', cg.Component, i2c.I2CDevice)
@@ -11,8 +15,8 @@ ES7210Microphone = es7210_ns.class_('ES7210Microphone', microphone.Microphone)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ES7210Component),
-    cv.Optional('sample_rate', default=16000): cv.int_,
-    cv.Optional('bits_per_sample', default=16): cv.int_,
+    cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_,
+    cv.Optional(CONF_BITS_PER_SAMPLE, default=16): cv.int_,
 }).extend(i2c.i2c_device_schema(0x40))
 
 async def to_code(config):
@@ -20,8 +24,10 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    var.set_sample_rate(config['sample_rate'])
-    var.set_bits_per_sample(config['bits_per_sample'])
+    if CONF_SAMPLE_RATE in config:
+        cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
+    if CONF_BITS_PER_SAMPLE in config:
+        cg.add(var.set_bits_per_sample(config[CONF_BITS_PER_SAMPLE]))
 
     mic = cg.new_Pvariable(config['id'])
     await microphone.register_microphone(mic, config)
