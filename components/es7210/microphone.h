12f88397b2b36fc7f4ea3ac5794f47517dde889c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 17:59:26 +0100

    Update microphone.h

--- components/es7210/microphone.h
+++ components/es7210/microphone.h
@@ -12,10 +12,7 @@ class ES7210Microphone : public microphone::Microphone {
   void set_es7210(ES7210Component *es7210) { this->es7210_ = es7210; }
 
  protected:
-  // Supprimer le `override` pour is_ready_() si la fonction n'est pas virtuelle dans la classe parente
   bool is_ready_() const { return true; }
-
-  // Correction de la signature de `read_chunk_()`, elle doit être correctement déclarée
   int read_chunk_(int16_t *buffer, size_t length) override;
 
  private:
@@ -25,4 +22,5 @@ class ES7210Microphone : public microphone::Microphone {
 
 }  // namespace es7210
 }  // namespace esphome
+
 
