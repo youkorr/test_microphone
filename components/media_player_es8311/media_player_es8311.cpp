bc024d28f282f41a7f5688e8bc33d4301849c450
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 23:15:17 +0100

    Update media_player_es8311.cpp

--- components/media_player_es8311/media_player_es8311.cpp
+++ components/media_player_es8311/media_player_es8311.cpp
@@ -9,6 +9,7 @@ static const char *const TAG = "media_player_es8311";
 void MediaPlayerES8311::setup() {
   ESP_LOGCONFIG(TAG, "Setting up MediaPlayerES8311...");
   ESP_LOGCONFIG(TAG, "  Sample Rate: %d", this->sample_rate_);
+  this->es8311_->initialize();
 }
 
 void MediaPlayerES8311::dump_config() {
@@ -23,26 +24,31 @@ void MediaPlayerES8311::loop() {
 void MediaPlayerES8311::play() {
   ESP_LOGI(TAG, "Playing media...");
   this->state = media_player::MEDIA_PLAYER_STATE_PLAYING;
+  this->es8311_->start();
 }
 
 void MediaPlayerES8311::stop() {
   ESP_LOGI(TAG, "Stopping playback...");
   this->state = media_player::MEDIA_PLAYER_STATE_IDLE;
+  this->es8311_->stop();
 }
 
 void MediaPlayerES8311::pause() {
   ESP_LOGI(TAG, "Pausing playback...");
   this->state = media_player::MEDIA_PLAYER_STATE_PAUSED;
+  this->es8311_->pause();
 }
 
 void MediaPlayerES8311::set_volume(float volume) {
   ESP_LOGI(TAG, "Setting volume: %.2f", volume);
   this->volume_ = volume;
+  this->es8311_->set_volume(volume);
 }
 
 void MediaPlayerES8311::mute(bool mute) {
   ESP_LOGI(TAG, "Mute: %s", mute ? "ON" : "OFF");
   this->is_muted_ = mute;
+  this->es8311_->set_mute(mute);
 }
 
 void MediaPlayerES8311::set_media_source(const std::string &source) {
