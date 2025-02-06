a808dc7bf3bae91a2d4466097ee6e725aa0d6b48
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 18:38:47 +0100

    Update microphone.h

--- components/es7210/microphone.h
+++ components/es7210/microphone.h
@@ -1,22 +1,18 @@
-#pragma once
-
-#include "esphome/components/microphone/microphone.h"  // Inclure la classe de base
-#include "es7210.h"
-
 namespace esphome {
 namespace es7210 {
 
-class ES7210Microphone : public microphone::Microphone {
- public:
-  bool is_ready_() const override { return true; }  // Implémentation de la méthode
-  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
+class ES7210Microphone : public esphome::sensor::Sensor {
+public:
+  bool is_ready_() const override {
+    return true;  // Implémentation de la méthode
+  }
 
- private:
-  ES7210Component *es7210_{nullptr};
+  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
 };
 
 }  // namespace es7210
 }  // namespace esphome
+
 
 
 
