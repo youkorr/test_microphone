eb807c7f8d21963feedeb3b56bbb90f8737c2a3c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 18:32:09 +0100

    Update microphone.h

--- components/es7210/microphone.h
+++ components/es7210/microphone.h
@@ -1,27 +1,23 @@
 #pragma once
 
-#include "esphome/components/microphone/microphone.h"
+#include "esphome/components/microphone/microphone.h"  // Inclure la classe de base
 #include "es7210.h"
-#include "driver/i2s.h"
 
 namespace esphome {
 namespace es7210 {
 
 class ES7210Microphone : public microphone::Microphone {
  public:
-  void set_es7210(ES7210Component *es7210) { this->es7210_ = es7210; }
-
- protected:
-  bool is_ready_() const override { return true; }
-  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration correcte de la méthode
+  bool is_ready_() const override { return true; }  // Implémentation de la méthode
+  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
 
  private:
   ES7210Component *es7210_{nullptr};
-  i2s_port_t i2s_port_ = I2S_NUM_0;
 };
 
 }  // namespace es7210
 }  // namespace esphome
+
 
 
 
