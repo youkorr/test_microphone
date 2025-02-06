1f8a646256f9318d805981c8357bf4699522a7ff
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 23:14:37 +0100

    Update __init__.py

--- components/media_player_es8311/__init__.py
+++ components/media_player_es8311/__init__.py
@@ -3,8 +3,9 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 from esphome.components import media_player
 from esphome.components.i2s_audio import I2SAudioOut
+from ..es8311 import ES8311
 
-DEPENDENCIES = ["media_player", "i2s_audio"]
+DEPENDENCIES = ["media_player", "i2s_audio", "es8311"]
 
 media_player_es8311_ns = cg.esphome_ns.namespace("media_player_es8311")
 MediaPlayerES8311 = media_player_es8311_ns.class_("MediaPlayerES8311", media_player.MediaPlayer, cg.Component)
@@ -12,6 +13,7 @@ MediaPlayerES8311 = media_player_es8311_ns.class_("MediaPlayerES8311", media_pla
 CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MediaPlayerES8311),
     cv.Required("i2s_audio"): cv.use_id(I2SAudioOut),
+    cv.Required("es8311"): cv.use_id(ES8311),
     cv.Optional("sample_rate", default=48000): cv.int_range(min=8000, max=96000),
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -22,5 +24,9 @@ async def to_code(config):
     
     i2s_audio = await cg.get_variable(config["i2s_audio"])
     cg.add(var.set_i2s_audio(i2s_audio))
+    
+    es8311 = await cg.get_variable(config["es8311"])
+    cg.add(var.set_es8311(es8311))
+    
     cg.add(var.set_sample_rate(config["sample_rate"]))
 
