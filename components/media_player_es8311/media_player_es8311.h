bf30838baa92d83c1583c625edba59e65d7aa362
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 22:40:49 +0100

    Update media_player_es8311.h

--- components/media_player_es8311/media_player_es8311.h
+++ components/media_player_es8311/media_player_es8311.h
@@ -1,15 +1,9 @@
-// media_player_es8311.h
 #pragma once
-
 #include "esphome/components/media_player/media_player.h"
-#include "esphome/components/i2s_audio/i2s_audio.h"
-#include "esphome/components/audio_dac/audio_dac.h"
-#include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
-namespace es8311 {
+namespace media_player_es8311 {
 
 class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
  public:
@@ -23,14 +17,8 @@ class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
   void set_volume(float volume) override;
   void mute(bool mute) override;
   void set_media_source(const std::string &source);
-
- protected:
-  void process_audio_();
-
-  std::string current_source_;
-  bool is_muted_ = false;
-  float volume_ = 1.0f;
 };
 
-}  // namespace es8311
+}  // namespace media_player_es8311
 }  // namespace esphome
+
