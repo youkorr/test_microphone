dce3c46f13289c6ea71dbe290ee49cc4cc540aa8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 19:07:15 +0100

    Update es7210.h

--- components/es7210/es7210.h
+++ components/es7210/es7210.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ES7210_H
+#define ES7210_H
 
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
@@ -8,22 +9,20 @@ namespace esphome {
 namespace es7210 {
 
 class ES7210Component : public Component, public i2c::I2CDevice {
- public:
+public:
   void setup() override;
   void dump_config() override;
-  
+
   void set_sample_rate(uint32_t rate) { sample_rate_ = rate; }
   void set_bits_per_sample(uint8_t bits) { bits_per_sample_ = bits; }
-  void set_i2s_port(i2s_port_t port) { i2s_port_ = port; }
 
   bool configure_i2s();
 
- private:
+private:
   uint32_t sample_rate_ = 16000;
   uint8_t bits_per_sample_ = 16;
   i2s_port_t i2s_port_ = I2S_NUM_0;
 
-  // Detailed ES7210 register configuration
   static constexpr uint8_t ES7210_RESET_REG = 0x00;
   static constexpr uint8_t ES7210_MAINCLK_REG = 0x01;
   static constexpr uint8_t ES7210_POWER_REG = 0x02;
@@ -32,11 +31,17 @@ class ES7210Component : public Component, public i2c::I2CDevice {
 
   bool write_register(uint8_t reg, uint8_t value);
   uint8_t read_register(uint8_t reg);
-  
+
   void initialize_codec();
   void configure_analog_path();
+
+public:
+  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
 };
 
 }  // namespace es7210
 }  // namespace esphome
+
+#endif  // ES7210_H
+
 
