48db845710c3715cfc1766d69eccff7ddc89659e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 01:26:26 +0100

    Update __init__.py

--- components/media_player/__init__.py
+++ components/media_player/__init__.py
@@ -15,8 +15,9 @@ MediaPlayer = media_player_ns.class_("MediaPlayer", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MediaPlayer),
     cv.Required(CONF_NAME): cv.string,
-    # Utilisez directement l'ID du composant i2s_audio sans référence de classe
-    cv.Required("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),  # <-- Changement clé ici
+    cv.Required("i2s_audio_id"): cv.use_id(i2s_audio.I2SAudioComponent),
+    cv.Required("es7210_id"): cv.use_id(i2s_audio.ES7210Component),  # Ajout du codec ES7210
+    cv.Required("es8311_id"): cv.use_id(i2s_audio.ES8311Component),  # Ajout du codec ES8311
 }).extend(cv.COMPONENT_SCHEMA)
 
 @coroutine_with_priority(100.0)
@@ -28,3 +29,10 @@ async def to_code(config):
     # Lier i2s_audio via son ID générique
     i2s_audio_var = await cg.get_variable(config["i2s_audio_id"])
     cg.add(var.set_i2s_audio(i2s_audio_var))
+
+    # Lier les codecs ES7210 et ES8311
+    es7210_var = await cg.get_variable(config["es7210_id"])
+    cg.add(var.set_es7210(es7210_var))
+
+    es8311_var = await cg.get_variable(config["es8311_id"])
+    cg.add(var.set_es8311(es8311_var))
