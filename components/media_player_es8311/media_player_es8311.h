a7d3136ebacd8968fb04df7771409485c2515bf2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-07 00:49:56 +0100

    Update media_player_es8311.h

--- components/media_player_es8311/media_player_es8311.h
+++ components/media_player_es8311/media_player_es8311.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "esphome/components/media_player/media_player.h"
 #include "esphome/core/component.h"
+#include "esphome/components/audio_dac/audio_dac.h" //This has to be included.
+#include "esphome/components/output/binary_output.h" //This has to be included
 
 namespace esphome {
 namespace media_player_es8311 {
 
 class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
  public:
+  MediaPlayerES8311() {}
+
   void setup() override;
   void loop() override;
   void dump_config() override;
@@ -17,6 +21,16 @@ class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
   void set_volume(float volume) override;
   void mute(bool mute) override;
   void set_media_source(const std::string &source);
+
+  void set_audio_dac(audio_dac::AudioDAC *dac) { dac_ = dac; } //This has to be included
+  void set_speaker(output::BinaryOutput *speaker) { speaker_ = speaker; } //This has to be included.
+  void set_sample_rate(uint32_t sample_rate) { sample_rate_ = sample_rate; } //This has to be included
+
+ protected:
+  audio_dac::AudioDAC *dac_{nullptr}; //This has to be included
+  output::BinaryOutput *speaker_{nullptr}; //This has to be included
+  uint32_t sample_rate_{16000}; //This has to be included
+
 };
 
 }  // namespace media_player_es8311
