71d143d839610dab5cfe4a934a7fdd8923789f68
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 19:44:25 +0100

    Update es7210.h

--- components/es7210/es7210.h
+++ components/es7210/es7210.h
@@ -4,9 +4,12 @@
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
 #include "driver/i2s.h"
+#include "microphone.h"
 
 namespace esphome {
 namespace es7210 {
+
+class ES7210Microphone;  // Forward declaration
 
 class ES7210Component : public Component, public i2c::I2CDevice {
 public:
@@ -17,6 +20,9 @@ public:
   void set_bits_per_sample(uint8_t bits) { bits_per_sample_ = bits; }
 
   bool configure_i2s();
+  
+  // Make this public and change to non-override
+  int read_chunk_(int16_t *buffer, size_t length);
 
 private:
   uint32_t sample_rate_ = 16000;
@@ -34,9 +40,6 @@ private:
 
   void initialize_codec();
   void configure_analog_path();
-
-public:
-  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
 };
 
 }  // namespace es7210
