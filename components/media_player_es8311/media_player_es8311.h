63a701802e41f3153d85a28e669bb3eea5e3784c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 23:04:53 +0100

    Update media_player_es8311.h

--- components/media_player_es8311/media_player_es8311.h
+++ components/media_player_es8311/media_player_es8311.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "esphome/components/media_player/media_player.h"
 #include "esphome/core/component.h"
+#include "esphome/components/audio_dac/audio_dac.h"
 
 namespace esphome {
 namespace media_player_es8311 {
@@ -11,12 +12,19 @@ class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
   void loop() override;
   void dump_config() override;
 
+  void set_audio_dac(audio_dac::AudioDAC *audio_dac) { this->audio_dac_ = audio_dac; }
+  void set_sample_rate(uint32_t sample_rate) { this->sample_rate_ = sample_rate; }
+
   void play() override;
   void stop() override;
   void pause() override;
   void set_volume(float volume) override;
   void mute(bool mute) override;
   void set_media_source(const std::string &source);
+
+ protected:
+  audio_dac::AudioDAC *audio_dac_;
+  uint32_t sample_rate_;
 };
 
 }  // namespace media_player_es8311
