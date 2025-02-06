1c050010e29cedf05c2aec5ecc03b29cea211689
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 23:03:51 +0100

    Update __init__.py

--- components/media_player_es8311/__init__.py
+++ components/media_player_es8311/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 from esphome.components import media_player
+from esphome.components.audio_dac import AudioDAC
 
 DEPENDENCIES = ["media_player", "i2s_audio", "audio_dac"]
 
@@ -10,10 +11,16 @@ MediaPlayerES8311 = media_player_es8311_ns.class_("MediaPlayerES8311", media_pla
 
 CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MediaPlayerES8311),
+    cv.Required("audio_dac"): cv.use_id(AudioDAC),
+    cv.Optional("sample_rate", default=48000): cv.int_range(min=8000, max=96000),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await media_player.register_media_player(var, config)
+    
+    audio_dac = await cg.get_variable(config["audio_dac"])
+    cg.add(var.set_audio_dac(audio_dac))
+    cg.add(var.set_sample_rate(config["sample_rate"]))
 
