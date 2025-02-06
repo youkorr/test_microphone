52f79f0dfdbf5c9f5f8a2b547067dce626619023
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 19:43:14 +0100

    Update microphone.h

--- components/es7210/microphone.h
+++ components/es7210/microphone.h
@@ -1,12 +1,23 @@
 #ifndef MICROPHONE_H
 #define MICROPHONE_H
 
+#include "esphome/components/microphone/microphone.h"
+#include "esphome/core/component.h"
+
 namespace esphome {
 namespace es7210 {
 
-class ES7210Microphone : public esphome::sensor::Sensor {
+class ES7210Component;  // Forward declaration
+
+class ES7210Microphone : public microphone::Microphone {
 public:
-  int read_chunk_(int16_t *buffer, size_t length) override;  // Déclaration de la méthode
+  void set_es7210(ES7210Component* parent) { parent_ = parent; }
+  
+protected:
+  int read_chunk_(int16_t *buffer, size_t length) override;
+
+private:
+  ES7210Component* parent_ = nullptr;
 };
 
 }  // namespace es7210
