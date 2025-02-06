c785ff9de1fc84f8c9e799e45401ff1f7dab93c9
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 17:37:50 +0100

    Update sensor.py

--- components/es7210/sensor.py
+++ components/es7210/sensor.py
@@ -11,13 +11,13 @@ ES7210Microphone = es7210_ns.class_('ES7210Microphone', microphone.Microphone)
 
 CONF_SAMPLE_RATE = 'sample_rate'
 CONF_BITS_PER_SAMPLE = 'bits_per_sample'
-CONF_ID = 'id'  # Define CONF_ID explicitly
+CONF_ID = 'id'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ES7210Component),
     cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_,
     cv.Optional(CONF_BITS_PER_SAMPLE, default=16): cv.int_,
-    cv.Optional(CONF_ID, default='default_es7210'): cv.string,  # Define a default ID
+    cv.Optional(CONF_ID, default='default_es7210'): cv.string,
 }).extend(i2c.i2c_device_schema(0x40))
 
 async def to_code(config):
@@ -28,7 +28,6 @@ async def to_code(config):
     sample_rate = config.get(CONF_SAMPLE_RATE)
     bits_per_sample = config.get(CONF_BITS_PER_SAMPLE)
 
-    # Set the sample rate and bits per sample
     if sample_rate is not None:
         cg.add(var.set_sample_rate(sample_rate))
     
@@ -38,4 +37,6 @@ async def to_code(config):
     mic = cg.new_Pvariable(config[CONF_ID])
     await microphone.register_microphone(mic, config)
     cg.add(mic.set_es7210(var))
+
+
 
