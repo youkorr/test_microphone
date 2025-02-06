aa72b92ee719b9c4b96b9732846280a51e7697fe
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 22:30:17 +0100

    Create __init__.py

---
+++ components/media_player_es8311/__init__.py
@@ -0,0 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+from esphome.components import media_player
+
+DEPENDENCIES = ["media_player", "i2s_audio", "audio_dac"]
+
+media_player_es8311_ns = cg.esphome_ns.namespace("media_player_es8311")
+MediaPlayerES8311 = media_player_es8311_ns.class_("MediaPlayerES8311", media_player.MediaPlayer, cg.Component)
+
+CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(MediaPlayerES8311),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await media_player.register_media_player(var, config)
