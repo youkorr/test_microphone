51048e3711ef06fba02cf5b03892d0f12c4b6f1c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-06 23:49:12 +0100

    Update media_player_es8311.h

--- components/media_player_es8311/media_player_es8311.h
+++ components/media_player_es8311/media_player_es8311.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "esphome/components/media_player/media_player.h"
 #include "esphome/core/component.h"
-#include "esphome/components/i2s_audio/i2s_audio.h"
-#include "../es8311/es8311.h"
 
 namespace esphome {
 namespace media_player_es8311 {
@@ -13,22 +11,14 @@ class MediaPlayerES8311 : public media_player::MediaPlayer, public Component {
   void loop() override;
   void dump_config() override;
 
-  void set_i2s_audio(i2s_audio::I2SAudioOut *i2s_audio) { this->i2s_audio_ = i2s_audio; }
-  void set_es8311(es8311::ES8311 *es8311) { this->es8311_ = es8311; }
-  void set_sample_rate(uint32_t sample_rate) { this->sample_rate_ = sample_rate; }
-
   void play() override;
   void stop() override;
   void pause() override;
   void set_volume(float volume) override;
   void mute(bool mute) override;
   void set_media_source(const std::string &source);
-
- protected:
-  i2s_audio::I2SAudioOut *i2s_audio_;
-  es8311::ES8311 *es8311_;
-  uint32_t sample_rate_;
 };
 
 }  // namespace media_player_es8311
 }  // namespace esphome
+
