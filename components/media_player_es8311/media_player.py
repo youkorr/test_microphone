fc261263cd50d0e5ab97189b71279d78f3771fd0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-07 01:30:13 +0100

    Update media_player.py

--- components/media_player_es8311/media_player.py
+++ components/media_player_es8311/media_player.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import media_player
 
-# Custom component namespace
 media_player_es8311_ns = cg.esphome_ns.namespace("media_player_es8311")
 MediaPlayerES8311 = media_player_es8311_ns.class_(
     "MediaPlayerES8311", media_player.MediaPlayer, cg.Component
