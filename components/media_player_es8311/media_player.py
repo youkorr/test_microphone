9247292ecc62f756646009bc0230b45cb4864fdb
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-07 01:14:40 +0100

    Update media_player.py

--- components/media_player_es8311/media_player.py
+++ components/media_player_es8311/media_player.py
@@ -1,35 +1,16 @@
 import esphome.codegen as cg
 from esphome.components import media_player
 from esphome.core import CORE, ID
-from esphome.components import audio_dac  # Change the library
+from esphome.components import audio_dac #Change the library
 from esphome.components import output
 
 # Custom component namespace
-DEPENDENCIES = ["audio_dac", "output"]  # Change the dependency
-MULTI_CONF = True  # Important for using the id()
-CONF_AUDIO_DAC_ID = "audio_dac_id"  # Change for audiodac
+DEPENDENCIES = ["audio_dac", "output"] # Change the dependency
+MULTI_CONF = True # Important for using the id()
+CONF_AUDIO_DAC_ID = "audio_dac_id" # Change for audio_dac
 CONF_OUTPUT_ID = "output_id"
 CODEOWNERS = ["@YouKorr"]
+
 media_player_es8311_ns = cg.esphome_ns.namespace("media_player_es8311")
 MediaPlayerES8311 = media_player_es8311_ns.class_(
     "MediaPlayerES8311", media_player.MediaPlayer, cg.Component
-)
-
-@cg.coroutine
-def new_es8311_media_player(config):
-    var = cg.new_Pvariable(config[CONF_ID], template=MediaPlayerES8311())
-    yield cg.register_component(var, config)
-    yield media_player.register_media_player(var, config)
-
-    # Get the audio_dac audio component and set it on the media player
-    dac = yield cg.get_variable(config[CONF_AUDIO_DAC])  # Set the correct Audio Dac settings.
-    cg.add(var.set_audio_dac(dac))  # Call setter method to pass in AudioDac object
-
-    # Get the speaker component and set it on the media player
-    speaker = yield cg.get_variable(config[CONF_OUTPUT])  # Ensure the correct configuration.
-    cg.add(var.set_speaker(speaker))  # Call setter method to pass in speaker object.
-
-    cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
-    #Implement the sample_rate within media_player_es8311.h
-
-    return var
