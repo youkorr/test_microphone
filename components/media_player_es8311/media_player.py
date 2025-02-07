85dc9bba3882a8e095230b7213f75b571e4c4f55
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-07 01:16:20 +0100

    Update media_player.py

--- components/media_player_es8311/media_player.py
+++ components/media_player_es8311/media_player.py
@@ -1,16 +1,8 @@
 import esphome.codegen as cg
 from esphome.components import media_player
-from esphome.core import CORE, ID
-from esphome.components import audio_dac #Change the library
-from esphome.components import output
 
 # Custom component namespace
-DEPENDENCIES = ["audio_dac", "output"] # Change the dependency
-MULTI_CONF = True # Important for using the id()
-CONF_AUDIO_DAC_ID = "audio_dac_id" # Change for audio_dac
-CONF_OUTPUT_ID = "output_id"
-CODEOWNERS = ["@YouKorr"]
-
 media_player_es8311_ns = cg.esphome_ns.namespace("media_player_es8311")
 MediaPlayerES8311 = media_player_es8311_ns.class_(
     "MediaPlayerES8311", media_player.MediaPlayer, cg.Component
+)
